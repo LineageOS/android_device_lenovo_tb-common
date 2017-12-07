7c3a0be8ded121a54d88d203ed52bf99ed5a6ad3
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    tb-common: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- tb.mk
+++ tb.mk
@@ -168,6 +168,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common
