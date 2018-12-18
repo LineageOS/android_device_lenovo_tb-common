6c3079200dcc88dc8abb8234a4ed8ecd92bc6cab
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    tb-common: Enabling sound trigger V2_2 for msm8953 targets
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- tb.mk
+++ tb.mk
@@ -55,7 +55,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
