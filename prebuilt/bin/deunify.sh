8f89f12d88f684d4daba0d2af2badd039eeb3461
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-10-27 18:39:27 +0100

    tb-common: deunify: adapt for new layout
    Change-Id: Ic52bee870575745ef51c17bf0cdbf226780975b1

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -9,22 +9,22 @@ do
 done
 
 # Move variant-specific blobs
-mv /system/vendor/firmware/variant/$baseband/a506_zap* /system/vendor/firmware/
-mv /system/vendor/firmware/variant/$baseband/goodixfp* /system/vendor/firmware/
-rm -rf /system/vendor/firmware/variant
+mv /system/system/vendor/firmware/variant/$baseband/a506_zap* /system/system/vendor/firmware/
+mv /system/system/vendor/firmware/variant/$baseband/goodixfp* /system/system/vendor/firmware/
+rm -rf /system/system/vendor/firmware/variant
 
 # Remove telephony files for wifi variant
 if [ "$baseband" == "apq" ]; then 
-    rm -rf /system/app/datastatusnotification
-    rm -rf /system/app/messaging
-    rm -rf /system/app/QtiTelephonyService
-    rm -rf /system/app/SecureElement
-    rm -rf /system/app/SimAppDialog
-    rm -rf /system/app/Stk
-    rm -rf /system/priv-app/CarrierConfig
-    rm -rf /system/priv-app/CellBroadcastReceiver
-    rm -rf /system/priv-app/Dialer
-    rm -rf /system/priv-app/Telecom
-    rm -rf /system/priv-app/TelephonyProvider
-    rm -rf /system/priv-app/TeleService
+    rm -rf /system/system/app/datastatusnotification
+    rm -rf /system/system/app/messaging
+    rm -rf /system/system/app/QtiTelephonyService
+    rm -rf /system/system/app/SecureElement
+    rm -rf /system/system/app/SimAppDialog
+    rm -rf /system/system/app/Stk
+    rm -rf /system/system/priv-app/CarrierConfig
+    rm -rf /system/system/priv-app/CellBroadcastReceiver
+    rm -rf /system/system/priv-app/Dialer
+    rm -rf /system/system/priv-app/Telecom
+    rm -rf /system/system/priv-app/TelephonyProvider
+    rm -rf /system/system/priv-app/TeleService
 fi
