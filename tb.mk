410199ad0a1db400ed0875acb707ab30654dc660
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-11-04 21:11:19 +0100

    DNM: tb-common: update bluetooth blobs
    Change-Id: I793ed2357145d523fce26ca2c181b438150c2c72

--- tb.mk
+++ tb.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# QMI
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
