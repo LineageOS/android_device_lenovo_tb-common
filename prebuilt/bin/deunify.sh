b1b4a5385875504b302b379a8cc5f2e7ca3032bd
codeworkx <daniel.hillenbrand@codeworkx.de>
2020-03-01 17:54:45 +0100

    tb-common: deunify: don't remove TelephonyProvider
    Is required by gms
    
    Change-Id: Ibff6d586c620882e83806b958dfc222694ca90de

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -25,6 +25,5 @@ if [ "$baseband" == "apq" ]; then
     rm -rf /system/system/priv-app/CellBroadcastReceiver
     rm -rf /system/system/priv-app/Dialer
     rm -rf /system/system/priv-app/Telecom
-    rm -rf /system/system/priv-app/TelephonyProvider
     rm -rf /system/system/priv-app/TeleService
 fi
