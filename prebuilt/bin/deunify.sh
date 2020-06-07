378367981ebf47575255ae2aac60b8ea8366e4dd
codeworkx <daniel.hillenbrand@codeworkx.de>
2020-06-07 16:41:32 +0200

    tb-common: deunify: do not remove telephony specific components
    * DPM depends on dialer and will loop when it's not available
    
    Change-Id: I0f0afc59c8fe0a1fb54e805a503c4bfe47405f55

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -12,17 +12,3 @@ done
 mv /system/system/vendor/firmware/variant/$baseband/a506_zap* /system/system/vendor/firmware/
 mv /system/system/vendor/firmware/variant/$baseband/goodixfp* /system/system/vendor/firmware/
 rm -rf /system/system/vendor/firmware/variant
-
-# Remove telephony files for wifi variant
-if [ "$baseband" == "apq" ]; then 
-    rm -rf /system/system/app/datastatusnotification
-    rm -rf /system/system/app/messaging
-    rm -rf /system/system/app/QtiTelephonyService
-    rm -rf /system/system/app/SecureElement
-    rm -rf /system/system/app/SimAppDialog
-    rm -rf /system/system/app/Stk
-    rm -rf /system/system/priv-app/CarrierConfig
-    rm -rf /system/system/priv-app/CellBroadcastReceiver
-    rm -rf /system/system/priv-app/Dialer
-    rm -rf /system/system/priv-app/TeleService
-fi
